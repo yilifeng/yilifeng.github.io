e15c155a9155c16cc6a6c8c4a73cda089ed4a8af
yilifeng <746733042@qq.com>
2018-11-03 20:23:48 -0700

    add blog

--- my-project/src/unit/Blog.vue
+++ my-project/src/unit/Blog.vue
@@ -3,79 +3,56 @@
     <el-col :span="5"><div class="grid-content bg-purple">
       <aside-aside name="qwert"></aside-aside>
     </div></el-col>
-    <el-col :span="19"><div class="grid-content bg-purple-light">
-      <el-form :model="form" ref="form" label-width="50px" :rules="rule">
-        <el-form-item label="姓    名" prop="username">
-        <el-input type="text" v-model="form.username" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="密    码" prop="password">
-          <el-input type="password" v-model="form.password" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="确认密码" prop="password2">
-          <el-input type="password" v-model="form.password2" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="create">提交</el-button>
-          <el-button @click="resetForm">重置</el-button>
-        </el-form-item>
-      </el-form>
-    </div></el-col>
+    <el-col :span="19">
+      <el-row>
+        <el-button v-if="!showBlogAdd" :onclick="add">添加</el-button>
+      </el-row>
+      <el-row>
+        <blog-add v-if="showBlogAdd"></blog-add>
+        <blog-add v-if="showBlogList"></blog-add>
+        <blog-add v-if="showBlogShow"></blog-add>
+      </el-row>
+    </el-col>
   </el-row>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
 import Aside from '@/components/Aside'
+import BlogAdd from '@/unit/BlogAdd'
+import BlogList from '@/unit/BlogList'
+import BlogShow from '@/unit/BlogShow'
 export default {
   components: {
-    'aside-aside': Aside
+    'aside-aside': Aside,
+    'blog-add': BlogAdd,
+    'blog-list': BlogList,
+    'blog-show': BlogShow
   },
   data () {
-    var validate = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('此项为必填项'))
-      } else if (rule.field === 'password2' && value !== this.form.password) {
-        callback(new Error('两次输入密码不一致!'))
-      } else {
-        callback()
-      }
-    }
     return {
+      editorContent: '',
       showAside: true,
-      form: {
-        username: ''
-      },
-      rules: {
-        username: [
-          { pattern: /^[a-zA-Z_][a-zA-Z_0-9]{5,29}$/, trigger: 'blur', message: '用户名仅支持字母数字下划线, 长度为6-30字符, 不能以数字开头' },
-          { validator: validate, trigger: 'blur' }
-        ]
-      }
+      showBlogAdd: false,
+      showBlogList: true,
+      showBlogShow: false
     }
   },
   methods: {
     ...mapActions([
       'createUser'
     ]),
-    create () {
-      let res = this.createUser(this.form)
-      console.log('create res:')
-      console.log(res)
+    getContent () {
+      alert(this.editorContent)
     },
-    resetForm () {
-      this.$refs.resetFields()
+    add () {
+      this.showBlogAdd = true
     }
   }
 }
 </script>
 
 <style scoped>
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 100%;
-  }
   .el-aside {
     background-color: #D3DCE6;
     color: #333;
---
+++ my-project/src/unit/BlogShow.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="grid-content bg-purple-light">
+    <el-row>
+      <div id="editor"></div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import Editor from 'wangeditor'
+import 'wangeditor/release/wangEditor.min.css'
+export default {
+  data () {
+    return {
+      editorContent: ''
+    }
+  },
+  methods: {
+    ...mapActions([
+      'createUser'
+    ]),
+    getContent () {
+      alert(this.editorContent)
+    }
+  },
+  mounted () {
+    this.editor = new Editor('#editor')
+    this.editor.create()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
